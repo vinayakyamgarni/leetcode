2794bf9468158183ed56b1d0a620849cc78fecb2
vinayakyamgarni <vinayakyamgarni032004@gmail.com>
2026-07-04 10:48:50 +0530

    Time: 160 ms (28.1%), Space: 42.7 MB (94.38%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,11 +5,13 @@ class Solution {
         int m = board.length;
         int n = board[0].length;
 
+        int[][] track = new int[m][n];
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
 
                 if (board[i][j] == word.charAt(0)
-                        && found(board, i, j, 0, m, n, word)) {
+                        && found(board, track, i, j, 0, m, n, word)) {
                     return true;
                 }
             }
@@ -18,25 +20,25 @@ class Solution {
         return false;
     }
 
-    public boolean found(char[][] board, int r, int c, int id,
+    public boolean found(char[][] board, int[][] track,
+                         int r, int c, int id,
                          int m, int n, String word) {
 
         if (id == word.length())
             return true;
 
         if (r < 0 || c < 0 || r >= m || c >= n ||
-                board[r][c] != word.charAt(id))
+            board[r][c] != word.charAt(id) || track[r][c] == 1)
             return false;
 
-        char temp = board[r][c];
-        board[r][c] = '#';
+        track[r][c] = 1;
 
-        boolean ans = found(board, r - 1, c, id + 1, m, n, word) ||
-                      found(board, r + 1, c, id + 1, m, n, word) ||
-                      found(board, r, c - 1, id + 1, m, n, word) ||
-                      found(board, r, c + 1, id + 1, m, n, word);
+        boolean ans = found(board, track, r - 1, c, id + 1, m, n, word) ||
+                      found(board, track, r + 1, c, id + 1, m, n, word) ||
+                      found(board, track, r, c - 1, id + 1, m, n, word) ||
+                      found(board, track, r, c + 1, id + 1, m, n, word);
 
-        board[r][c] = temp;
+        track[r][c] = 0; // Backtrack
 
         return ans;
     }
