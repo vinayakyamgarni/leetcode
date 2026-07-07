d1bd41350a44061b6af6b9833d441099e9bae2ed
vinayakyamgarni <vinayakyamgarni032004@gmail.com>
2026-07-07 11:16:15 +0530

    Time: 0 ms (100%), Space: 44 MB (94.52%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -15,21 +15,21 @@ class Solution {
 
         while (l1 != null && l2 != null) {
             if (l1.val <= l2.val) {
-                curr.next = l1;
+                dummy.next = l1;
                 l1 = l1.next;
             } else {
-                curr.next = l2;
+                dummy.next = l2;
                 l2 = l2.next;
             }
-            curr = curr.next;
+            dummy = dummy.next;
         }
 
         if (l1 != null) {
-            curr.next = l1;
+            dummy.next = l1;
         } else {
-            curr.next = l2;
+            dummy.next = l2;
         }
 
-        return dummy.next;
+        return curr.next;
     }
 }
